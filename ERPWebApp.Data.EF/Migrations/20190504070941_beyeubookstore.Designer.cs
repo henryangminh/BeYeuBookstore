5ea87dd74e76bad68235d5649169d1d18110c7de
Hồ Trường Nhật Minh <hippore114@gmail.com>
2019-05-04 17:10:43 +0700

    Install and update some nuget packages

--- ERPWebApp.Data.EF/Migrations/20190504070941_beyeubookstore.Designer.cs
+++ ERPWebApp.Data.EF/Migrations/20190504070941_beyeubookstore.Designer.cs
@@ -9,8 +9,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BeYeuBookstore.Data.EF.Migrations
 {
     [DbContext(typeof(ERPDbContext))]
-    [Migration("20190502083339_init")]
-    partial class init
+    [Migration("20190504070941_beyeubookstore")]
+    partial class beyeubookstore
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
